2432491387224bdc23c327fd5b19a1c49d81924e
Arno Debelle <debelle.a@gmail.com>
2022-02-22 12:58:51 +0100

    puting occasion as an optional filed for car

--- app/models/car.rb
+++ app/models/car.rb
@@ -3,6 +3,7 @@ class Car < ApplicationRecord
   has_many :car_categories, dependent: :destroy
   has_many :reviews, dependent: :destroy
   has_many :categories, through: :car_categories
-  validates :brand, :model, :occasion, :description, presence: true
+  validates :brand, :model, :description, presence: true
   validates :price, :capacity, presence: true, numericality: { only_integer: true }
+  attribute :occasion, default: "N/A"
 end
