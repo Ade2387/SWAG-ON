b8573f3bb3a34ea836618cbbb18e365e2fa83d37
oleg deru <olegderu98@gmail.com>
2022-02-22 17:02:49 +0100

    saving

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  get '/navbar-brand', to: 'pages#homepage'
   get '/dashboard', to: 'pages#dashboard'
   resources :cars do
     resources :reviews, only: %i[new create]
