cd97bf1ae56560039de6c527aadb61cce1dde7a0
Arno Debelle <debelle.a@gmail.com>
2022-02-23 16:30:10 +0100

    attemp heroku workaroubnd

--- app/models/car.rb
+++ app/models/car.rb
@@ -5,6 +5,5 @@ class Car < ApplicationRecord
   has_many :categories, through: :car_categories
   validates :brand, :model, :description, presence: true
   validates :price, :capacity, presence: true, numericality: { only_integer: true }
-  attribute :occasion, default: "N/A"
-  attribute :image, default: "https://i.pinimg.com/564x/77/b8/1b/77b81bd9b49ea414cc73c06481c965e1.jpg"
+
 end
